b5b3619ed886565b5338431beefa71cf6f59a246
Alex <bn_sole@163.com>
2025-10-26 17:53:00 +0800

    added public subnet a/b

--- infra/deploy/vpc.tf
+++ infra/deploy/vpc.tf
@@ -11,3 +11,64 @@ resource "aws_internet_gateway" "main" {
     "Name" = "${local.prefix}-main"
   }
 }
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
+
+}
+
+resource "aws_route_table" "public_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public_a"
+  }
+
+}
+
+resource "aws_route_table_association" "public_a" {
+  route_table_id = aws_route_table.public_a.id
+  subnet_id      = aws_subnet.public_a.id
+}
+
+resource "aws_route" "public_a" {
+  route_table_id         = aws_route_table.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
+
+}
+
+resource "aws_route_table" "public_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public_b"
+  }
+
+}
+
+resource "aws_route_table_association" "public_b" {
+  route_table_id = aws_route_table.public_b.id
+  subnet_id      = aws_subnet.public_b.id
+}
+
+resource "aws_route" "public_b" {
+  route_table_id         = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
